92bc4d2882586364c405e5531e91d0a23de43c3f
chrisdhodges12 <102702357+chrisdhodges12@users.noreply.github.com>
2022-09-13 20:56:52 -0400

    server.js and auth.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,44 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//serve static assets
+if( process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/'));
+});
+
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+    // integrate Apollo server with  Express as middleware
+    server.applyMiddleware({ app });
+
+    db.once('open', () => {
+        app.listen(PORT, () => {
+          console.log(`API server running on port ${PORT}!`);
+        //Test GQL API here
+          console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+        })
+    })
+  };
+
+
+startApolloServer(typeDefs, resolvers);
