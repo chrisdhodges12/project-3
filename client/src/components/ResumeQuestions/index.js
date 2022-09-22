a6d317fb4a8860a0bc53e01e3a1526d2eb1c2c6f
Booker King <btking1@my.waketech.edu>
2022-09-22 12:39:58 -0400

    accept merge from main

--- client/src/components/ResumeQuestions/index.js
+++ client/src/components/ResumeQuestions/index.js
@@ -2,11 +2,8 @@ import React, { useState } from "react";
 
 import { useMutation } from "@apollo/client";
 import { UPDATE_RESUME } from "../../utils/mutations";
-<<<<<<< HEAD
 import { QUERY_RESUMES, QUERY_ME } from "../../utils/queries";
 import { makeStyles, Typography, Grid } from "@material-ui/core";
-=======
->>>>>>> main
 
 const useStyles = makeStyles((theme) => ({
   mainBox: {
